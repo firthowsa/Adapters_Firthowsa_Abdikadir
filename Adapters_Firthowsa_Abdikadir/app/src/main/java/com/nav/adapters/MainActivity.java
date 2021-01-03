4f226d95ba642980262142ec58e0fa098a3921dc
firthowsa <36509759+firthowsa@users.noreply.github.com>
2021-01-03 18:50:25 +0300

    Add files via upload

---
+++ Adapters_Firthowsa_Abdikadir/app/src/main/java/com/nav/adapters/MainActivity.java
@@ -0,0 +1,35 @@
+package com.nav.adapters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.programs_array,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+    }
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+
+        parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+}
+
